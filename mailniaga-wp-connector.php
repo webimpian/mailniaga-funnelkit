e7de154bd4a8019e26aba7c0ca4e62f387063989
Web Impian <info@khairulimran.com>
2024-10-11 18:10:31 +0800

    cleanup

---
+++ mailniaga-wp-connector.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: MailNiaga SMTP API
+ * Plugin URI: https://mailniaga.com/
+ * Description: A simple SMTP plugin for WordPress using MailNiaga API
+ * Version: 1.3.0.beta-11072024
+ * Author: Firdaus Azizi
+ * Author URI: https://firz.my
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain: mailniaga-smtp
+ * Domain Path: /languages
+ * Requires at least: 5.0
+ * Requires PHP: 8.0
+ * Tested up to: 6.5
+ */
+// If this file is called directly, abort.
+if (!defined('WPINC')) {
+	die;
+}
